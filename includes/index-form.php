abee5aa456e04ffb52720d69156df6ae6149cfbd
Gerben Pellekaan <70696498+Gerben-Pellekaan@users.noreply.github.com>
2021-03-23 10:30:18 +0100

    Add files via upload

---
+++ includes/index-form.php
@@ -0,0 +1,19 @@
+<form action="index.php" method="post">
+	<label for="huisdier">Welk dier zou je nooit als huisdier willen hebben?</label><input type="text" name="huisdier">
+	<br><br>
+	<label for="persoon">Wie is de belangrijkste persoon in je leven?</label><input type="text" name="persoon">
+	<br><br>
+	<label for="land">In welk land zou je graag willen wonen?</label><input type="text" name="land">
+	<br><br>
+	<label for="verveel">Wat doe je als je je verveelt?</label><input type="text" name="verveel">
+	<br><br>
+	<label for="speelgoed">Met welk speelgoed speelde je als kind het meest?</label><input type="text" name="speelgoed">
+	<br><br>
+	<label for="docent">Bij welke docent spijbel je het liefste?</label><input type="text" name="docent">
+	<br><br>
+	<label for="geld">Als je €100.000,- had, wat zou je dan kopen?</label><input type="text" name="geld">
+	<br><br>
+	<label for="bezigheid">Wat is je favoriete bezigheid?</label><input type="text" name="bezigheid">
+	<br><br>
+	<input type="submit" name="versturen" value="Versturen">
+</form>
---
+++ includes/onkunde-form.php
@@ -0,0 +1,17 @@
+<form action="onkunde.php" method="post">
+	<label for="willen-kunnen">Wat zou je graag willen kunnen?</label><input type="text" name="willen-kunnen">
+	<br><br>
+	<label for="persoon-opschieten">Met welk persoon kun je goed opschieten?</label><input type="text" name="persoon-opschieten">
+	<br><br>
+	<label for="favGetal">Wat is je favoriete getal?</label><input type="text" name="favGetal">
+	<br><br>
+	<label for="vakantie">Wat heb je altijd bij je als je op vakantie gaat?</label><input type="text" name="vakantie">
+	<br><br>
+	<label for="besteEigenschap">Wat is je beste persoonlijke eigenschap?</label><input type="text" name="besteEigenschap">
+	<br><br>
+	<label for="slechtsteEigenschap">Wat is je slechtste persoonlijke eigenschap?</label><input type="text" name="slechtsteEigenschap">
+	<br><br>
+	<label for="ergsteOverkomen">Wat is het ergste dat je kan overkomen?</label><input type="text" name="ergsteOverkomen">
+	<br><br>
+	<input type="submit" name="versturen" value="Versturen">
+</form>
